97eeacb999b558317f9546517e1c33afde465ee0
rudrodip <official.rudrodipsarker@gmail.com>
2024-02-27 11:48:06 +0600

    init

---
+++ README.md
@@ -0,0 +1,15 @@
+# minigrep
+
+Simple grep-like program written in Rust (exercise from official rust book)
+
+## Usage
+
+```bash
+minigrep <pattern> <file>
+```
+
+## Example
+
+```bash
+minigrep to poem.txt
+```
---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use minigrep::Config;
+use std::{env, process};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::build(&args).unwrap_or_else(|_err| {
+        eprintln!("Problem parsing arguments");
+        process::exit(1);
+    });
+
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    }
+}
